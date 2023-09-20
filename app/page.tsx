69b55d4d6713fd1f65178abf0e604781d319ed1f
RickyPrima30 <113336156+RickyPrima30@users.noreply.github.com>
2023-09-20 17:18:32 +0700

    complete UI phase 1 and giving assets image
    Navbar, Footer, Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 import Image from 'next/image'
 
+import { Hero } from '@/components'
+
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x pading-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Katalog Mobil</h1>
+          <p>Jelajahi Mobil yang mungkin anda sukai</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title="Fuel" />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,38 @@
+"use client";
+
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 export const Hero = () => {
+  const handleScroll = () => {
+
+  }
+
   return (
-    <div>Hero</div>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Cari, Booking, atau Rental mobil. Cepat dan memudahkan!
+        </h1>
+
+        <p className="hero__subtitle">
+          Sederhanakan pengalaman sewa mobil Anda dengan proses pemesanan kami yang mudah.
+        </p>
+
+        <CustomButton 
+        title="Explore mobil"
+        containerStyles="bg-primary-blue text-white
+        rounded-full mt-10"
+        handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default Hero
